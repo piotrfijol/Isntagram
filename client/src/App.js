d9abf3a1887b7e4a03ddba8794de30b9e6989e0f
piotrfijol <piotr.fijol1@gmail.com>
2022-10-21 05:11:56 +0200

    Include router with a few routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import React from 'react';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:name" element={<Profile />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
