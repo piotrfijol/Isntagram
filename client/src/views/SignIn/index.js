9c3f58d3e6cc91a97e0b1ce1b4427e65d7c35ba5
piotrfijol <piotr.fijol1@gmail.com>
2022-10-22 18:52:50 +0200

    Create sign in form

---
+++ client/src/views/SignIn/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import TextInput from '../../components/TextInput'
+import './SignIn.scss';
+
+export default function SignIn() {
+  return (
+    <div className="content-wrapper">
+      <div className="form-container">
+        <header>
+          <h1>Isntagram</h1>
+        </header>
+        <form method="POST">
+          <div className="form-row">
+            <TextInput 
+              id="login" 
+              name="login" 
+              label="Login" 
+            />
+          </div>
+          <div className="form-row">
+            <TextInput 
+              type="password"
+              id="password" 
+              name="password" 
+              label="Password" 
+            />
+          </div>
+          Don't have an account? <Link to="/signup">Sign up now</Link>.
+          <button>Sign in</button>
+        </form>
+      </div>
+    </div>
+  )
+}
