d0e0810101306e543fe74600464eb74da48a1006
piotrfijol <piotr.fijol1@gmail.com>
2022-10-23 21:01:39 +0200

    Move Home view to its own dir

---
+++ client/src/views/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from '../../components/Post'
+
+export default function Home() {
+
+  return (
+    <React.Fragment>
+      <Post />
+      <Post />
+      <Post />
+    </React.Fragment>
+  )
+}
