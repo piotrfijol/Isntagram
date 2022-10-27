ba711e09ad731c362e9828ca94ee8e87177cb6fc
piotrfijol <piotr.fijol1@gmail.com>
2022-10-27 13:50:56 +0200

    Init application server project

---
+++ server/app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var apiRouter = require('./routes/api');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+
+  // render the error page
+  err.statusCode = err.status || 500;
+  res.status(err.statusCode);
+  res.json(err);
+
+});
+
+module.exports = app;
