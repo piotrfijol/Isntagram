c74d8a4622fc6785905a4f135ae5309c4efeca72
piotrfijol <piotr.fijol1@gmail.com>
2022-10-27 11:03:25 +0200

    Create grid layout for explore page

---
+++ client/src/views/Explore/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Explore.scss';
+
+export default function Explore() {
+  return (
+    <div className="posts-gallery">
+        <div className="posts-gallery__thumbnail">
+                <img 
+                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="A woman taking a picture"
+                />
+        </div>
+        <div className="posts-gallery__thumbnail">
+                <img 
+                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="A woman taking a picture"
+                />
+        </div>
+        <div className="posts-gallery__thumbnail">
+                <img 
+                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="A woman taking a picture"
+                />
+        </div>
+        <div className="posts-gallery__thumbnail">
+                <img 
+                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="A woman taking a picture"
+                />
+        </div>
+    </div>
+  )
+}
