1bc534c0551d0b7989ab5700f6f8fff1993318c9
piotrfijol <piotr.fijol1@gmail.com>
2022-10-30 11:58:41 +0100

    Hash passwords and do DB check for username and email

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,21 +1,32 @@
 const userModel = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const createUser = (req, res) => {
     const { username, password, email } = req.body;
 
-    let user = new userModel({
-        username,
-        password,
-        email
-    });
+    userModel.find().where({username}).or({email}).exec()
+    .then(async (data) => {
+        if(data.length)
+            return res.send("The username or email is already taken")
+        else {
 
-    user.save()
-        .then((data) => {
-            res.send("Account has been created successfully!");
-        })
-        .catch((err) => {
-            res.send("Error has occured!");
-        });
+            const hashedPassword = await bcrypt.hash(password, 12);
+
+            let user = new userModel({
+                username,
+                password: hashedPassword,
+                email
+            });
+
+            user.save()
+            .then((data) => {
+                res.send("Account has been created successfully!");
+            })
+            .catch((err) => {
+                res.send("Error has occured!");
+            });
+        }
+    });
 };
 
 
