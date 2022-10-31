fd574b178a5e9cace83f02ef0604858c522b588e
piotrfijol <piotr.fijol1@gmail.com>
2022-10-31 12:56:21 +0100

    Change a link to user avatar img placeholder

--- client/src/components/UserHeader/index.js
+++ client/src/components/UserHeader/index.js
@@ -5,7 +5,7 @@ export default function UserHeader({ user, className }) {
   return (
     <div className={"header " + className }>
         <div className="header__avatar">
-            <img src="https://scontent.cdninstagram.com/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=1&_nc_ohc=4x80AR8wRasAX-qGMCl&edm=AOL1moYAAAAA&ccb=7-5&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-5&oh=00_AT-7G1uHKUqykZf9VLLRR2TefJogffV8W3Lz_USoQewDRw&oe=635940CF&_nc_sid=97d328" alt="a profile picture"/>
+            <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="a profile picture"/>
         </div>
         <p className="header__username">Username</p>
     </div>
