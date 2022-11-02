2023ca1055ebe3dbfa178179fdcbb5bed2545fb7
piotrfijol <piotr.fijol1@gmail.com>
2022-11-02 11:18:18 +0100

    Link SignIn page with a backend

--- client/src/views/SignIn/index.js
+++ client/src/views/SignIn/index.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import TextInput from '../../components/TextInput'
 import './SignIn.scss';
 
 export default function SignIn() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sendSignInForm = (ev) => {
+    ev.preventDefault();
+
+    fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    }).then(data => data.json()).then(jsonData => {
+      localStorage.setItem("_jwt", jsonData.token);
+      alert("Logged in");
+      window.location.replace("/");
+    })
+  };
+
   return (
     <div className="content-wrapper">
       <div className="form-container">
         <header>
           <h1 className="logo">Isntagram</h1>
         </header>
-        <form method="POST">
+        <form method="POST" onSubmit={sendSignInForm}>
           <div className="form-row">
             <TextInput 
               id="login" 
               name="login" 
               label="Login" 
+              value={username}
+              onChange={(ev) => setUsername(ev.currentTarget.value)}
             />
           </div>
           <div className="form-row">
@@ -24,6 +49,8 @@ export default function SignIn() {
               id="password" 
               name="password" 
               label="Password" 
+              value={password}
+              onChange={(ev) => setPassword(ev.currentTarget.value)}
             />
           </div>
           <p className="neglible">Don't have an account? <Link to="/signup">Sign up now</Link>.</p>
