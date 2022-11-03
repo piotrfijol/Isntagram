9cf6e1c8d06a40af505c141ec86e4d47727c7d21
piotrfijol <piotr.fijol1@gmail.com>
2022-11-03 13:41:12 +0100

    Change error response messages

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,15 +3,23 @@ require("dotenv").config();
 
 exports.verifyJWT = async (req, res, next) => {
     try {
-        let result = jwt.verify(req.headers['authorization'].split(' ')[1], process.env.ACCESS_TOKEN_SECRET);
+        const {authorization: authorizationHeader} = req.headers;
+        if(!authorizationHeader) {
+            return res.status(401).json({
+                message: "Unauthorized",
+                statusCode: 401
+            });
+        }
+
+        let result = jwt.verify(authorizationHeader.split(' ')[1], process.env.ACCESS_TOKEN_SECRET);
         if(result) {
             return next();
         } 
     } catch(err) {
-        return res.status(401).json({
-            message: "Invalid token",
-            statusCode: 401
-        });
+        return res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        })
     }
         
 }
