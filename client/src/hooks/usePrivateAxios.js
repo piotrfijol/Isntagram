04df2c58a00a15df139cbae5c69fac5cdb19af76
piotrfijol <piotr.fijol1@gmail.com>
2022-11-05 19:13:43 +0100

    Handle unsuccessful attempt of acquiring a new accesToken

--- client/src/hooks/usePrivateAxios.js
+++ client/src/hooks/usePrivateAxios.js
@@ -1,16 +1,22 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { axiosPrivate } from '../api/axios'
 import { useAuth } from './useAuth';
+import { useRefreshToken } from './useRefreshToken';
 
 const usePrivateAxios = () => {
     const accessToken = localStorage.getItem('_jwt');
-    const [auth, setAuth] = useAuth();
+    const {auth, setAuth} = useAuth();
+    const navigate = useNavigate();
+    const refresh = useRefreshToken();
   
     useEffect(() => {
         const requestInterceptor = axiosPrivate.interceptors.request.use(
             request => {
-                if(!request.headers?.Authorization)
-                    request.headers['Authorization'] = `Bearer ${auth.accessToken}`;
+                if(auth && !request.headers['Authorization']) {
+                    request.headers = {};
+                    request.headers['Authorization'] = "Bearer " + auth?.accessToken;
+                }
                 return request;
             },
             (err) => Promise.reject(err)
@@ -20,18 +26,31 @@ const usePrivateAxios = () => {
             response => response,
             async (invalidResponse) => {
                 const request = invalidResponse.config;
-                if(invalidResponse.response?.status === 403 && !request.requestedRefresh) {
+                if(invalidResponse.response?.status === 403 
+                    || invalidResponse.response?.status === 401
+                    && !request.requestedRefresh) {
+
                     request.requestedRefresh = true;
-                    const newAccessToken = await axiosPrivate.get("/api/refresh-token");
+                    
+                    const data = await refresh();
+                    const newAccessToken = data.accessToken;
+
                     if(newAccessToken) {
+                        request.headers = {}
+                        request.headers['Authorization'] = "Bearer " + newAccessToken;
                         setAuth(prev => {
                             return {
                                 ...prev,
+                                username: data.username,
                                 accessToken: newAccessToken
                             }
                         })
+
+                        return axiosPrivate.request(request) 
+                    } else {
+                        navigate("/signin");
+                        return;
                     }
-                    return axiosPrivate(request);
                 }
                 return Promise.reject(invalidResponse)
             }
@@ -41,7 +60,7 @@ const usePrivateAxios = () => {
             axiosPrivate.interceptors.request.eject(requestInterceptor);
             axiosPrivate.interceptors.response.eject(responseInterceptor);
         }
-    }, []);
+    }, [auth, setAuth]);
 
     return axiosPrivate;
 }
