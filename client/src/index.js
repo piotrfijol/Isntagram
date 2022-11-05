7e48dace043173429fd7aac1503401c19efabb85
piotrfijol <piotr.fijol1@gmail.com>
2022-11-05 19:15:27 +0100

    Wrap App component with a Router
    This makes it possible to use useSetup hook inside the App component.

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AuthProvider>
   </React.StrictMode>
 );
