f22adc7279004cf05838ef5d150cb69687e7164e
piotrfijol <piotr.fijol1@gmail.com>
2022-11-07 12:37:00 +0100

    Initialize posts controller

---
+++ server/controllers/post.js
@@ -0,0 +1,25 @@
+
+const createPost = (req, res) => {
+
+};
+
+const getPosts = (req, res) => {
+
+};
+
+const updatePost = (req, res) => {
+
+};
+
+const removePost = (req, res) => {
+
+};
+
+module.exports = {
+    createPost,
+    getPosts,
+    updatePost,
+    removePost
+}
+
+
