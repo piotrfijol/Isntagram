9d8f896e4faad2248d2f215b0cdd9ea51dcfea90
piotrfijol <piotr.fijol1@gmail.com>
2022-11-07 13:58:29 +0100

    Remove unused imports

--- client/src/views/SignOut.js
+++ client/src/views/SignOut.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import usePrivateAxios from '../hooks/usePrivateAxios';
@@ -7,7 +7,7 @@ export const SignOut = () => {
   
     const axiosPrivate = usePrivateAxios();
     const navigate = useNavigate();
-    const {auth, setAuth} = useAuth();
+    const {setAuth} = useAuth();
 
     useEffect(() => {
         axiosPrivate.post("/api/logout")
