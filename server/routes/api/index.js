b43434afb385dc039f197cbfc04d1fa12820ff61
piotrfijol <piotr.fijol1@gmail.com>
2022-11-07 13:59:28 +0100

    Include post routes in the api

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./auth");
+const postRoutes = require("./post");
 
 router.use(authRoutes);
+router.use(postRoutes);
 
 module.exports = router;
