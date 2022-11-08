5178fd1d38ae27df9a0836cbe0a03f97e6371785
piotrfijol <piotr.fijol1@gmail.com>
2022-11-08 23:52:54 +0100

    Include multer dependency

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -2,15 +2,26 @@ const express = require("express");
 const router = express.Router();
 const { verifyJWT } = require("../../middleware/verifyJWT");
 const postController = require("../../controllers/post");
+const multer = require("multer");
+const upload = multer();
 
 router.use(verifyJWT);
 
 router.post("/post",
+    upload.single("file"),
     postController.createPost
 );
 
+router.get("/posts/explore",
+    postController.getAnyPosts
+)
+
 router.get("/posts",
     postController.getPosts
+);
+
+router.get("/post/:id",
+    postController.getPost
 );
 
 router.put("/post/:id",
