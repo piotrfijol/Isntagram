00fb7e08a9462b0139817e64c13a40736fdbdea0
piotrfijol <piotr.fijol1@gmail.com>
2022-11-08 23:53:28 +0100

    Create post model

---
+++ server/models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        description: {
+            type: String,
+            maxLength: 360
+        },
+        imgURL: {
+            type: String,
+            required: true
+        },
+        tags: [String],
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("Post", postSchema)
