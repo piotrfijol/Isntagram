6165b4504d37059af05f4ec5010ec8175f8ca7a8
piotrfijol <piotr.fijol1@gmail.com>
2022-11-08 23:58:40 +0100

    Fetch posts to display on /explore page

--- client/src/views/Explore/index.js
+++ client/src/views/Explore/index.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import usePrivateAxios from '../../hooks/usePrivateAxios';
 import './Explore.scss';
 
 export default function Explore() {
+
+    const [posts, setPosts] = useState([]);
+    const axiosPrivate = usePrivateAxios();
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const {signal} = controller;
+
+        axiosPrivate.get("/api/posts/explore", {
+            signal
+        }).then((response) => {
+            setPosts(response.data.posts);
+        });
+
+        return () => {
+            controller.abort();
+        }
+    }, [setPosts]);
+
   return (
     <div className="posts-gallery">
-        <div className="posts-gallery__thumbnail">
-                <img 
-                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A woman taking a picture"
-                />
-        </div>
-        <div className="posts-gallery__thumbnail">
-                <img 
-                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A woman taking a picture"
-                />
-        </div>
-        <div className="posts-gallery__thumbnail">
-                <img 
-                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A woman taking a picture"
-                />
-        </div>
-        <div className="posts-gallery__thumbnail">
-                <img 
-                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A woman taking a picture"
-                />
-        </div>
+        {
+            posts.map((post) => {
+                return (
+                    <Link to={`/p/${post._id}`} >
+                        <div className="posts-gallery__thumbnail">
+                            <img 
+                                src={post.imgURL}
+                            />
+                        </div>
+                    </Link>
+                ) 
+            })
+        }
     </div>
   )
 }
