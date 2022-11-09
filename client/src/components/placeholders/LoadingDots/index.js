63c0442c4a8406f453fef7c5b255294dac19d185
piotrfijol <piotr.fijol1@gmail.com>
2022-11-09 15:55:07 +0100

    Add dots loading animation component

---
+++ client/src/components/placeholders/LoadingDots/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoadingDots.scss';
+
+export const LoadingDots = () => {
+  return (
+    <div className="loading-dots">
+        <div className="loading-dots__dot" />
+        <div className="loading-dots__dot" />
+        <div className="loading-dots__dot" />
+    </div>
+  )
+}
