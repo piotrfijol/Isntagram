3fe0a7f1c60ad91803ae2af31125f2c9ab5991d8
piotrfijol <piotr.fijol1@gmail.com>
2022-11-09 16:01:13 +0100

    Wrap UserHeader component with a Link

--- client/src/components/UserHeader/index.js
+++ client/src/components/UserHeader/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './UserHeader.scss'
 
-export default function UserHeader({ user, className }) {
+export default function UserHeader({ user={username: 'User'}, className }) {
   return (
-    <div className={"header " + className }>
-        <div className="header__avatar">
-            <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="a profile picture"/>
-        </div>
-        <p className="header__username">Username</p>
-    </div>
+    <Link to={`/profile/${user.username}`}>
+      <div className={"header " + className }>
+          <div className="header__avatar">
+              <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="a profile picture"/>
+          </div>
+          <p className="header__username">{user.username}</p>
+      </div>
+    </Link>
   )
 }
 
