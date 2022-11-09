931cad4bbe4136d7ff46b422187af28f7900fd2e
piotrfijol <piotr.fijol1@gmail.com>
2022-11-09 16:03:45 +0100

    Fetch post's details from the server

--- client/src/views/PostPreview/index.js
+++ client/src/views/PostPreview/index.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { LoadingDots } from '../../components/placeholders/LoadingDots';
 import Post from '../../components/Post'
+import usePrivateAxios from '../../hooks/usePrivateAxios';
 
 export default function PostPreview() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState(null);
+  const params = useParams();
+  const axiosPrivate = usePrivateAxios();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setIsLoading(true);
+
+    axiosPrivate(`/api/post/${params.postId}`, {
+      signal
+    })
+      .then((response) => {
+        setPost(response.data);
+        setIsLoading(false);
+      })
+
+      return () => {
+        controller.abort();
+        setIsLoading(false);
+      }
+
+  }, [setPost]);
+  
   return (
-    <Post 
-      variant="preview"
-    />
+      isLoading ? 
+        <LoadingDots />
+      : (
+      <Post 
+        post={post}
+        variant="preview"
+      />
+    )
   )
 }
