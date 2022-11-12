102c3dc36138b17f99a3688f6da2fc61088e3f04
piotrfijol <piotr.fijol1@gmail.com>
2022-11-12 10:25:58 +0100

    Fetch user data follow related

--- client/src/views/Profile/index.js
+++ client/src/views/Profile/index.js
@@ -1,43 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { FollowButton } from '../../components/FollowButton';
+import { useAuth } from '../../hooks/useAuth';
+import usePrivateAxios from '../../hooks/usePrivateAxios';
 import './Profile.scss'
 
 export default function Profile() {
+
+    const [userData, setUserData] = useState(null);
+    const [followers, setFollowers] = useState([]);
+    const [following, setFollowing] = useState([]);
+    const {name: username} = useParams();
+    const axiosPrivate = usePrivateAxios();
+    const {auth} = useAuth();
+
+    useEffect(() => {
+        axiosPrivate.get(`/api/user/${username}/followers`)
+            .then((response) => {
+                setFollowers(response.data.followers);
+            }).catch((err) => {
+                console.error(err);
+            });
+
+        axiosPrivate.get(`/api/user/${username}/following`)
+            .then((response) => {
+                setFollowing(response.data.following);
+            }).catch((err) => {
+                console.error(err);
+            });
+
+        axiosPrivate.get(`/api/user/${username}`)
+            .then((response) => {
+                setUserData(response.data);
+            }).catch((err) => {
+                console.error(err);
+            })
+    }, [username]);
+
   return (
+    userData ?
     <div className="profile-container">
         <div className="profile">
             <div className="profile__avatar">
                 <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="a profile picture"/>
             </div>
             <div className="profile__info">
-                <p className="profile__info__username">Username</p>
+                <p className="profile__info__username">{userData.username}</p>
                 <div className="profile__info__details">
-                    <p>Posts 0</p>
-                    <p>Followers 0</p>
-                    <p>Following 0</p>
+                    <p>Posts {userData.posts.length}</p>
+                    <p>Followers {followers.length}</p>
+                    <p>Following {following.length}</p>
                 </div>
                 <p className="profile__info__bio">Follow me on other social media: Snapchat.. Tiktok.. Yt..</p>
             </div>
+            <div>
+                {userData.username === auth.username 
+                ? null 
+                : <FollowButton username={username} /> 
+            }
+            </div>
         </div>
         <div className="profile-gallery">
-            <div className="profile-gallery__image">
-                <img 
-                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A woman taking a picture"
-                />
-            </div>
-            <div className="profile-gallery__image">
-                <img 
-                    src="https://images.pexels.com/photos/416024/pexels-photo-416024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A harbor with boats in it"
-                />
-            </div>  
-            <div className="profile-gallery__image">
-                <img 
-                    src="https://images.pexels.com/photos/2272940/pexels-photo-2272940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    alt="A woman taking a picture"
-                />
-            </div>
+            {userData.posts.map((post) => {
+                return (
+                    <Link to={`/p/${post._id}`} key={post._id}>
+                        <div className="profile-gallery__image">
+                            <img 
+                                src={post.imgURL[480]}
+                                alt="A woman taking a picture"
+                            />
+                        </div>
+                    </Link>
+                )
+            })}
         </div>
     </div>
+    : null
   )
 }
