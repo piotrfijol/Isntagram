c4d90fbc5ee4cd944558e7b04fe333317b45db7c
piotrfijol <piotr.fijol1@gmail.com>
2022-11-12 10:24:20 +0100

    Push follow logic to the FollowButton component

--- client/src/components/FollowButton/index.js
+++ client/src/components/FollowButton/index.js
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaCheck, FaPlus } from 'react-icons/fa';
+import usePrivateAxios from '../../hooks/usePrivateAxios';
 import './FollowButton.scss';
 
-export const FollowButton = ({ isFollowing, onClick }) => {
+export const FollowButton = ({ username }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [isFetching, setIsFetching] = useState(true);
+  const axiosPrivate = usePrivateAxios();
+
+  useEffect(() => {
+    axiosPrivate.get(`/api/user/${username}/follow`)
+    .then((response) => {
+        setIsFollowing(response.data.isFollowing);
+    }).catch((err) => {
+
+    }).finally(() => {
+      setIsFetching(false);
+    });
+  }, [setIsFollowing, username]);
+
+  const handleFollow = (ev) => {
+    if(isFetching) return;
+
+    setIsFetching(true)
+    if(!isFollowing) {
+      axiosPrivate.put(`/api/user/${username}/follow`)
+        .then((data) => {
+          setIsFollowing(true);
+        }).catch((err) => {
+
+        }).finally(() => {
+          setIsFetching(false);
+        });
+        
+    } else {
+      axiosPrivate.delete(`/api/user/${username}/follow`)
+        .then((data) => {
+          setIsFollowing(false);
+        }).catch((err) => {
+
+        }).finally(() => {
+          setIsFetching(false);
+        });
+    }
+    
+  };
+  
   return (
     !isFollowing ? (
-        <button onClick={onClick} className="follow-btn">
+        <button onClick={handleFollow} className="follow-btn">
             <FaPlus className='follow-btn__icon' />
             Follow
         </button>
     ) : (
-        <button onClick={onClick} className="follow-btn follow-btn--active">
+        <button onClick={handleFollow} className="follow-btn follow-btn--active">
             <FaCheck className='follow-btn__icon' />
             Followed
         </button>
