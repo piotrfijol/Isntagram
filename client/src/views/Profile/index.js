8f5feacc2cfbda923ccc64e0d0767fb1e6075d0d
piotrfijol <piotr.fijol1@gmail.com>
2022-11-15 09:25:37 +0100

    Reduce a number of axios request on a Profile page

--- client/src/views/Profile/index.js
+++ client/src/views/Profile/index.js
@@ -9,33 +9,19 @@ import './Profile.scss'
 export default function Profile() {
 
     const [userData, setUserData] = useState(null);
-    const [followers, setFollowers] = useState([]);
-    const [following, setFollowing] = useState([]);
     const {name: username} = useParams();
     const axiosPrivate = usePrivateAxios();
     const {auth} = useAuth();
 
     useEffect(() => {
-        axiosPrivate.get(`/api/user/${username}/followers`)
-            .then((response) => {
-                setFollowers(response.data.followers);
-            }).catch((err) => {
-                console.error(err);
-            });
-
-        axiosPrivate.get(`/api/user/${username}/following`)
-            .then((response) => {
-                setFollowing(response.data.following);
-            }).catch((err) => {
-                console.error(err);
-            });
-
+        
         axiosPrivate.get(`/api/user/${username}`)
             .then((response) => {
                 setUserData(response.data);
             }).catch((err) => {
                 console.error(err);
-            })
+            });
+
     }, [username]);
 
   return (
@@ -49,8 +35,8 @@ export default function Profile() {
                 <p className="profile__info__username">{userData.username}</p>
                 <div className="profile__info__details">
                     <p>Posts {userData.posts.length}</p>
-                    <p>Followers {followers.length}</p>
-                    <p>Following {following.length}</p>
+                    <p>Followers {userData.followers.count}</p>
+                    <p>Following {userData.following.count}</p>
                 </div>
                 <p className="profile__info__bio">Follow me on other social media: Snapchat.. Tiktok.. Yt..</p>
             </div>
