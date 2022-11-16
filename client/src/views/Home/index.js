367b47b89c9988f6814f3fbf775ca372909587d3
piotrfijol <piotr.fijol1@gmail.com>
2022-11-16 23:51:44 +0100

    Fetch user feed from the server

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../../components/Post'
+import usePrivateAxios from '../../hooks/usePrivateAxios';
 
 export default function Home() {
 
+  const axiosPrivate = usePrivateAxios();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const aController = new AbortController();
+    const signal = aController.signal;
+
+    axiosPrivate.get("/api/posts", {
+      signal
+    }).then((response) => {
+      setPosts(response.data.posts);
+    });
+
+    return () => {
+      aController.abort();
+    }
+  }, []);
+
   return (
     <React.Fragment>
-      <Post />
-      <Post />
-      <Post />
+      {
+        posts.map((post) => {
+          return <Post post={post} />; 
+        })
+      }
     </React.Fragment>
   )
 }
