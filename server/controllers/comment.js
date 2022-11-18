7f830b3a0757c67844870989122527af0f0f540a
piotrfijol <piotr.fijol1@gmail.com>
2022-11-18 23:49:15 +0100

    Implement comment controller

---
+++ server/controllers/comment.js
@@ -0,0 +1,55 @@
+const commentModel = require("../models/comment");
+
+const createComment = (req, res) => {
+    const { postId } = req.params;
+
+    const comment = new commentModel({
+        post: postId,
+        user: req.user._id,
+        content: req.body.data.content
+    });
+
+    comment.save()
+        .then(async (document) => {
+            const {content, post, user} = await document.populate({path: "user", select: "username -_id"});
+
+            res.status(201).json({
+                statusCode: 201,
+                comment: {
+                    content,
+                    user,
+                    post
+                } 
+            });
+
+        }).catch((err) => {
+            return res.status(500).json({
+                statusCode: 500,
+                message: "Server internal error"
+            });
+        });
+};
+
+const getComments = (req, res) => {
+    const { postId } = req.params;
+    
+    commentModel.find({ post: postId }, "user post content")
+    .populate({path:"user", select:"username -_id"})
+        .then((comments) => {
+            return res.status(200).json({
+                statusCode: 200,
+                comments
+            })
+        }) 
+        .catch((err) => {
+            res.status(500).json({
+                statusCode: 500,
+                message: "Server Internal error"
+            })
+        });
+};
+
+module.exports = {
+    createComment,
+    getComments
+};
