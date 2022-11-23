ddd6f09d5c23ea6c267babb8e09ac3f7d9a03f9c
piotrfijol <piotr.fijol1@gmail.com>
2022-11-23 23:49:48 +0100

    Create settings page component

---
+++ client/src/components/Settings/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import './Settings.scss'
+
+export const Settings = () => {
+  return (
+    <div className='settings-container'>
+        <div className='settings-container__tabs'>
+            <Link to="/settings/edit">
+                Edit profile
+            </Link>
+        </div>
+        <div className='settings-container__settings'>
+            <Outlet />
+        </div>
+    </div>
+  )
+}
