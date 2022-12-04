661e4d201a4eb82feecda5fae6f9c0c6e9d7a961
piotrfijol <piotr.fijol1@gmail.com>
2022-12-04 23:53:57 +0100

    Add title to popup component

--- client/src/components/PopUp/index.js
+++ client/src/components/PopUp/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import "./PopUp.scss";
 import { FaPlus } from "react-icons/fa";
 
-export const PopUp = ({ children, isVisible = false, onClose }) => {
+export const PopUp = ({ title, children, isVisible = false, onClose }) => {
     
     return (
     isVisible ? (
         <div className="popup">
-            <div className="popup__box" onKeyDown={}>
+            <div className="popup__box" >
                 <div className="popup__box__cross" onClick={onClose}>
                     <FaPlus />
                 </div>                
+                <h2 style={{textAlign: 'center', fontSize: '1.2rem'}}>{title}</h2>
                 { children }
             </div>
         </div>
