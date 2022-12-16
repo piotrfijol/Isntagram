6d54b15ffe6c017356b7b6fc4b25efaa248bb7d4
piotrfijol <piotr.fijol1@gmail.com>
2022-12-16 23:49:00 +0100

    Replace most of Home component logic with component

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,67 +1,50 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../../components/Post'
 import usePrivateAxios from '../../hooks/usePrivateAxios'
+import { InfiniteScroll } from '../../components/InfiniteScroll';
 import { LoadingDots } from '../../components/placeholders/LoadingDots'
 
 export default function Home() {
 
   const axiosPrivate = usePrivateAxios();
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const [seenAll, setSeenAll] = useState(false);
 
-  const fetchPosts = (signal, page) => {
-      
+  const fetchPosts = (page, signal) => {
+
     if(!Number.isInteger(page)) throw new TypeError("Page parameter must be a number")
 
-    return axiosPrivate.get(`/api/posts?p=${page}`, {
-      signal
-    })
+    const config = signal ? {signal} : {};
+
+    return axiosPrivate.get(`/api/posts?p=${page}`, config)
+    .then((response) => {
+      const {posts} = response.data;
+
+      if(posts.length > 0) {
+        setPosts((prev) => [...prev, ...posts]);
+        setPage((page) => page+1);
+      } else {
+        setSeenAll(true);
+      }
+    }).catch((err) => {
+      console.error(err);
+    });
 
   };
   
+
+
   const scrollHandler = (ev) => {
-    let footer = document.querySelector(".lazyload-threshold").getBoundingClientRect();
-
-    if(footer.top <= window.innerHeight) {
-        if(!loading && !seenAll) {
-          setLoading(true);
-          axiosPrivate.get(`/api/posts?p=${page}`)
-          .then((response) => {
-            const {posts} = response.data;
-
-            if(posts.length > 0) {
-              setPosts((prev) => [...prev, ...posts]);
-              setPage((page) => page+1);
-            } else {
-              setSeenAll(true);
-              setLoading(false);
-            }
-          }).catch((err) => {
-            console.error(err);
-          }).finally(() => {
-            setLoading(false);
-          })
-        }
-    }
-  }
+    fetchPosts(page);
+  };
 
   useEffect(() => {
     const aController = new AbortController();
     const signal = aController.signal;
 
-    fetchPosts(signal, page).then((response) => {
-      setPage((page) => page+1);
-      setPosts((prev) =>  {
-       return [...prev, ...response.data.posts]
-      });
-    }).catch((err) => {
-      console.error(err);
-    }).finally(() => {
-      setLoading(false);
-    });
-
+    fetchPosts(page, signal);
+    
     return () => {    
       aController.abort();
     }
@@ -70,38 +53,24 @@ export default function Home() {
 
   return (
       (
-        
-        <div className="lazyload__container" style={{width: '100%', overflowY: 'scroll'}} onScrollCapture={scrollHandler}>
-          
-          {
-            posts.map((post) => {
-              return <Post key={post._id} post={post} />; 
-            })
-          }
-        
-          {
-            loading 
-              ? <LoadingDots />
-              : null
-          }
-          <div 
-            className="lazyload-threshold"
-            style={
-              {
-                width: "100%",
-                height: "50px",
-                marginTop: "70px"
-              }
-            }
-          ></div>
-          {
-            seenAll ? (
+        <InfiniteScroll 
+          itemsLength={posts.length}
+          handler={scrollHandler}
+          finalMessage={
               <div>
                 <p style={{textAlign: 'center'}} className="neglible">You've seen it all</p>
               </div>
-            ) : null
           }
-        </div>
+          loader={<LoadingDots />}
+          seenAll={seenAll}
+        >
+            {
+              posts.map((post) => {
+                return <Post key={post._id} post={post} />; 
+              })
+            }
+        </InfiniteScroll>          
+          
       )
   )
 }
