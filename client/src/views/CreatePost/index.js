30dede5755c2eca104b558417b7adf98263c0356
piotrfijol <piotr.fijol1@gmail.com>
2022-12-21 23:55:19 +0100

    Include tags in the create post form

--- client/src/views/CreatePost/index.js
+++ client/src/views/CreatePost/index.js
@@ -2,11 +2,12 @@ import React, { useRef, useState } from 'react'
 import TextInput from '../../components/TextInput'
 import "./CreatePost.scss";
 import usePrivateAxios from "../../hooks/usePrivateAxios";
+import { TagsInput } from '../../components/TagsInput';
 
 export const CreatePost = () => {
     
     const [description, setDescription] = useState("");
-    const [tags, setTags] = useState("");
+    const [tags, setTags] = useState([]);
     const fileInput = useRef(null);
     const axiosPrivate = usePrivateAxios();
 
@@ -16,7 +17,7 @@ export const CreatePost = () => {
         const formData = new FormData();
         formData.append("file", fileInput.current.files[0]);
         formData.append("description", description);
-        formData.append("tags", tags);
+        formData.append("tags", tags.join("#"));
 
         axiosPrivate.post("/api/post", formData, {
             headers: {
@@ -25,6 +26,11 @@ export const CreatePost = () => {
         }).then((response) => {
             
         });
+    };
+
+
+    const handleNewTag = (tags) => {
+        setTags(tags);
     };
 
   return (
@@ -38,6 +44,7 @@ export const CreatePost = () => {
         <label htmlFor="post-image" className="file-label">
         </label>
         <input ref={fileInput} type="file" id="post-image"/>
+        <TagsInput onCreate={handleNewTag}/>
         <button type="submit">Create post</button>
     </form>
   )
