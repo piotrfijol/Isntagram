c8a11846cf95a8016655a97865220e8904171533
piotrfijol <piotr.fijol1@gmail.com>
2022-12-22 17:16:50 +0100

    Implement tags controller to fetch all the posts specific to a given tag

---
+++ server/controllers/tags.js
@@ -0,0 +1,18 @@
+const postModel = require("../models/post");
+
+const getPostsByTag = async (req, res) => {
+    const {tag} = req.params;
+    const PAGE_SIZE = 3;
+    const page = req.query['p'];
+
+    const posts = await postModel.find({tags: tag}).populate("user", "-_id").sort({createdAt: -1}).skip(PAGE_SIZE * page).limit(PAGE_SIZE);
+
+    return res.status(200).json({
+        statusCode: 200,
+        posts
+    });
+};
+
+module.exports = {
+    getPostsByTag
+}
