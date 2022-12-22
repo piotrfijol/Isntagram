14e06e2082f13522f9c0f5be471808dda73b6111
piotrfijol <piotr.fijol1@gmail.com>
2022-12-22 17:19:44 +0100

    Create TagPosts page

---
+++ client/src/views/TagPosts/index.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import Post from '../../components/Post'
+import usePrivateAxios from '../../hooks/usePrivateAxios'
+import { InfiniteScroll } from '../../components/InfiniteScroll';
+import { LoadingDots } from '../../components/placeholders/LoadingDots'
+import { useParams } from 'react-router-dom';
+
+export default function TagPosts() {
+
+  const axiosPrivate = usePrivateAxios();
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [seenAll, setSeenAll] = useState(false);
+  const params = useParams();
+
+  const fetchPosts = (page, signal) => {
+
+    if(!Number.isInteger(page)) throw new TypeError("Page parameter must be a number")
+
+    const config = signal ? {signal} : {};
+
+    return axiosPrivate.get(`/api/tags/${params.tag}?p=${page}`, config)
+    .then((response) => {
+      const {posts} = response.data;
+
+      if(posts.length > 0) {
+        setPosts((prev) => [...prev, ...posts]);
+        setPage((page) => page+1);
+      } else {
+        setSeenAll(true);
+      }
+    }).catch((err) => {
+      console.error(err);
+    });
+
+  };
+  
+
+
+  const scrollHandler = (ev) => {
+    fetchPosts(page);
+  };
+
+  useEffect(() => {
+    const aController = new AbortController();
+    const signal = aController.signal;
+
+    fetchPosts(page, signal);
+    
+    return () => {    
+      aController.abort();
+    }
+  }, []);
+
+
+  return (
+      (
+        <InfiniteScroll 
+          itemsLength={posts.length}
+          handler={scrollHandler}
+          finalMessage={
+              <div>
+                <p style={{textAlign: 'center'}} className="neglible">You've seen it all</p>
+              </div>
+          }
+          loader={<LoadingDots />}
+          seenAll={seenAll}
+        >
+            {
+              posts.map((post) => {
+                return <Post key={post._id} post={post} />; 
+              })
+            }
+        </InfiniteScroll>          
+          
+      )
+  )
+}
