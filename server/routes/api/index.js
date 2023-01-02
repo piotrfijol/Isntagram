434b6b8c693a0d114350092cd10c6e7d106b1111
piotrfijol <piotr.fijol1@gmail.com>
2023-01-02 10:47:12 +0100

    Include a route for searching users and tags

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -6,11 +6,13 @@ const postRoutes = require("./post");
 const userRoutes = require("./user");
 const tagsRoutes = require("./tags");
 const accountRoutes = require("./account");
+const searchRoutes = require("./search");
 
 router.use(authRoutes);
 router.use(postRoutes);
 router.use(userRoutes);
 router.use(accountRoutes);
 router.use(tagsRoutes);
+router.use(searchRoutes);
 
 module.exports = router;
