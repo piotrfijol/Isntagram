9ff24a9b4ca5716b5b11b541ba132ebbeb01bc55
Piotr Fijol <piotr.fijol1@gmail.com>
2023-01-23 14:08:25 +0100

    Validate profile data

--- server/routes/api/account.js
+++ server/routes/api/account.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const upload = multer();
+const { body, validationResult } = require("express-validator");
 
 const accountController = require("../../controllers/account");
 
-router.post("/account/edit", 
+router.post("/account/edit",
+    body("biography")
+    .isLength({max: 350})
+    .withMessage("Biography can't be longer than 350 characters."),
+    (req, res, next) => {
+        const {errors} = validationResult(req);
+        if(errors.length > 0) {
+            return res.status(422).json({
+                status: 422,
+                message: errors[0].msg
+            });
+        }
+        
+        next();
+    },  
     accountController.edit
 );
 
