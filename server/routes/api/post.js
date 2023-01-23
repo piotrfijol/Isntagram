1d459a8c626563e85c9228a8ef687ff7058b7fe5
Piotr Fijol <piotr.fijol1@gmail.com>
2023-01-23 15:39:17 +0100

    Validate post's image extension

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -6,11 +6,32 @@ const likeController = require("../../controllers/like");
 const commentController = require("../../controllers/comment");
 const multer = require("multer");
 const upload = multer();
+const {body, validationResult} = require("express-validator");
+const path = require("path");
 
 router.use(verifyJWT);
 
 router.post("/post",
     upload.single("file"),
+    body("file")
+    .isEmpty()
+    .withMessage("Image file is required")
+    .custom((value, {req}) => {
+        if(req.file.mimetype === "image/jpeg")
+            return true;
+        else
+            return false;
+    }).withMessage("File must be of type .jpeg"),
+    ((req, res, next) => {
+        const {errors} = validationResult(req);
+        if(errors.length > 0) {
+            return res.status(422).json({
+                status: 422,
+                message: errors[0].msg
+            })
+        }
+        next();
+    }),
     postController.createPost
 );
 
